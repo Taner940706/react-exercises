4aceebd8959a69e865fa7aa20f974e00107e15f2
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-10 16:06:46 +0200

    course-information-exercise part 2

--- course-information-exercise/src/Content.js
+++ course-information-exercise/src/Content.js
@@ -1,6 +1,15 @@
+import Part from './Part'
+
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
 const Content = (props) => {
     return (
-        <><p>{props.part1} {props.exercise1}</p><p>{props.part2} {props.exercise2}</p><p>{props.part3} {props.exercise3}</p></>
+        <Part part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     )
 }
 
---
+++ course-information-exercise/src/Part.js
@@ -0,0 +1,7 @@
+const Part = (props) =>{
+    return (
+        <><p>{props.part1} {props.exercise1}</p><p>{props.part2} {props.exercise2}</p><p>{props.part3} {props.exercise3}</p></>
+    )
+}
+
+export default Part
