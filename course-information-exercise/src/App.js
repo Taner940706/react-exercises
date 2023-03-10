929506d408b9376b899b2f88eaab44331a590818
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-10 15:15:52 +0200

    course information exercise part one

---
+++ course-information-exercise/src/App.js
@@ -0,0 +1,89 @@
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
+
+
+
+// 1.course information, step1
+
+// The application that we will start working on in this exercise will be further developed in a few of the following exercises. In this and other upcoming exercise sets in this course, it is enough to only submit the final state of the application. If desired, you may also create a commit for each exercise of the series, but this is entirely optional.
+
+// Use create-react-app to initialize a new application. Modify index.js to match the following
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+// and App.js to match the following
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// and remove extra files (App.css, App.test.js, index.css, logo.svg, setupTests.js, reportWebVitals.js)).
+
+// Unfortunately, the entire application is in the same component. Refactor the code so that it consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component using props. Header takes care of rendering the name of the course, Content renders the parts and their number of exercises and Total renders the total number of exercises.
+
+// Define the new components in the file App.js.
+
+// The App component's body will approximately be as follows:
+
+// const App = () => {
+//   // const-definitions
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content ... />
+//       <Total ... />
+//     </div>
+//   )
+// }
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
---
+++ course-information-exercise/src/Content.js
@@ -0,0 +1,7 @@
+const Content = (props) => {
+    return (
+        <><p>{props.part1} {props.exercise1}</p><p>{props.part2} {props.exercise2}</p><p>{props.part3} {props.exercise3}</p></>
+    )
+}
+
+export default Content
