41696c19785bb6461a9bfe4fef1c5f1f4789ad48
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-15 19:12:03 +0200

    unicafe exercise part 1

---
+++ unicafe/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button type="button" onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button type="button" onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button type="button" onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+export default App;
