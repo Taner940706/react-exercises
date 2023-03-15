a5cbb6c9b6917fd0c8c72218c2673c84b4b22fea
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-15 19:17:59 +0200

    unicafe exercise part2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,5 +1,37 @@
 import { useState } from "react";
 
+
+// part 1
+// const App = () => {
+//   // save clicks of each button to its own state
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+//   return (
+//     <div>
+//       <h2>give feedback</h2>
+//       <button type="button" onClick={() => setGood(good + 1)}>
+//         good
+//       </button>
+//       <button type="button" onClick={() => setNeutral(neutral + 1)}>
+//         neutral
+//       </button>
+//       <button type="button" onClick={() => setBad(bad + 1)}>
+//         bad
+//       </button>
+//       <h2>statistics</h2>
+//       <p>good {good}</p>
+//       <p>neutral {neutral}</p>
+//       <p>bad {bad}</p>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -22,6 +54,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good * 1 + bad * -1) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad)}</p>
     </div>
   );
 };
